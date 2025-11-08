dce33132b1be32218e925176c97eb27dc90f9034
itsnotbubs <mbubs01@gmail.com>
2025-11-07 22:59:19 -0800

    Data Pull: built out functionality

--- data/data_pull.py
+++ data/data_pull.py
@@ -3,6 +3,8 @@ from pathlib import Path
 import sys
 import pandas as pd
 import requests
+import yfinance as yf
+import os
 
 sys.path.append(str(Path(__file__).resolve().parents[1]))
 from config.config import api_key, user, password
@@ -14,6 +16,7 @@ ivol.setLoginParams(username={user}, password={password})
 
 ivol.setLoginParams(apiKey=api_key)
 
+
 def grab_optionsymbol(
     snapshot_date: str,
     forward_date: str,
@@ -22,9 +25,12 @@ def grab_optionsymbol(
     strikeFrom: int,
     strikeTo: int,
 ):
-    getOptionsChain = ivol.setMethod("/equities/eod/option-series-on-date") # can be moved to main or collector
+    getOptionsChain = ivol.setMethod(
+        "/equities/eod/option-series-on-date"
+    )  # can be moved to main or collector
     optionsChain = getOptionsChain(
         symbol=f"{option}",
+        expFrom=f"{forward_date}",
         expTo=f"{forward_date}",
         strikeFrom=strikeFrom,
         strikeTo=strikeTo,
@@ -35,10 +41,7 @@ def grab_optionsymbol(
     return optionsChain
 
 
-def grab_optionprices(
-    snapshot_date: str,
-    optionsChain: pd.DataFrame
-):
+def grab_optionprices(snapshot_date: str, optionsChain: pd.DataFrame):
     allData = pd.DataFrame()
     getMarketData = ivol.setMethod("/equities/eod/single-stock-option")
 
@@ -54,19 +57,41 @@ def grab_optionprices(
         except requests.exceptions.RequestException as e:
             print(f"Error fetching data for {optionSymbol}: {e}")
     return allData
-    
 
-def collection():
-    #want to essentially loop over dates for a given underlying depending on a 
-    # the specified forward date ie in the paper its 28 days or forward month 
+
+def collection(forward_dates: [], underlying: str):
+    # want to essentially loop over dates for a given underlying depending on a
+    # the specified forward date ie in the paper its 28 days or forward month
     # this colleciton should just be a merge between calls and puts
-    pass
+    data = pd.DataFrame()
+    for forward_date in forward_dates:
+        print(forward_date)
+        snapshot_date = forward_date - pd.Timedelta(days=7)
+        print(snapshot_date)
+        price = int(underlying_price(snapshot_date, underlying))
+        symbols = grab_optionsymbol(
+            snapshot_date=snapshot_date.strftime("%Y-%m-%d"),
+            forward_date=forward_date.strftime("%Y-%m-%d"),
+            option="SPX",
+            type="C",
+            strikeFrom=price - 30,
+            strikeTo=price + 100,
+        )
+        option_NBBO = grab_optionprices(
+            snapshot_date=snapshot_date.strftime("%Y-%m-%d"), optionsChain=symbols
+        )
 
-def write():
-    #Takes collection and writes into a csv to ensure if anything breaks along the lines
-    # we have the data and can continue as needed 
-    # 
-    #  
+        data = pd.concat([data, option_NBBO], axis=0)
+    return data
+
+
+def write(data: pd.DataFrame, year: str):
+    # Takes collection and writes into a csv to ensure if anything breaks along the lines
+    # we have the data and can continue as needed
+    if os.path.exists("data/raw_data_" + year):
+        return data.to_csv("data/raw_data_" + year, mode="a", header=False, index=False)
+    else:
+        return data.to_csv("data/raw_data_" + year, mode="w", header=False)
 
 
 def put_call_parity():
@@ -74,13 +99,34 @@ def put_call_parity():
     pass
 
 
-
-option_data = grab_optionsymbol(option="SPX",
-    forward_date="2025-11-06",
-    strikeFrom=6700,
-    strikeTo=6750,
-    type="C",
-    snapshot_date="2025-11-05")
+def underlying_price(date: str, underlying: str):
+    return yf.download(underlying, start=date, end=date + pd.Timedelta(days=1))[
+        "Close"
+    ].values.flatten()[0]
 
 
-print(grab_optionprices(snapshot_date="2025-11-05", optionsChain=option_data))
+def allfridays(year):
+    return pd.date_range(start=str(year), end=str(year + 1), freq="W-FRI").tolist()
+
+
+# option_data = grab_optionsymbol(
+#     option="SPX",
+#     forward_date="2025-11-07",
+#     strikeFrom=6700,
+#     strikeTo=6750,
+#     type="C",
+#     snapshot_date="2025-10-31",
+# )
+
+# test = grab_optionprices(snapshot_date="2025-10-31", optionsChain=option_data)
+
+# print(test)
+# print(print(test['date']),test['expiration'])
+data_range = allfridays(2019)
+
+retrieve = collection(
+    forward_dates=data_range,
+    underlying="^SPX",
+)
+
+write(retrieve, "2019")
