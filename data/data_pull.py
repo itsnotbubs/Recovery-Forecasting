8a918e948f7f6377f75d5087a54d051edb8b306e
itsnotbubs <mbubs01@gmail.com>
2025-11-14 14:50:00 -0800

    data pull: completed api data extraction module

--- data/data_pull.py
+++ data/data_pull.py
@@ -5,93 +5,99 @@ import pandas as pd
 import requests
 import yfinance as yf
 import os
+import time
+from tqdm import tqdm
+from typing import Any
 
 sys.path.append(str(Path(__file__).resolve().parents[1]))
 from config.config import api_key, user, password
 
 
-print(f"Loaded API_KEY (len={len(api_key) if api_key else 0})")
-
-ivol.setLoginParams(username={user}, password={password})
-
-ivol.setLoginParams(apiKey=api_key)
-
 
 def grab_optionsymbol(
     snapshot_date: str,
+    expiry_start_date:str, 
     forward_date: str,
     option: str,
     type: str,
     strikeFrom: int,
     strikeTo: int,
+    getOptionsChain: Any
 ):
-    getOptionsChain = ivol.setMethod(
-        "/equities/eod/option-series-on-date"
-    )  # can be moved to main or collector
     optionsChain = getOptionsChain(
-        symbol=f"{option}",
-        expFrom=f"{forward_date}",
-        expTo=f"{forward_date}",
+        symbol=option,
+        expFrom=expiry_start_date,
+        expTo=forward_date,
         strikeFrom=strikeFrom,
         strikeTo=strikeTo,
-        callPut=f"{type}",
-        date=f"{snapshot_date}",
+        callPut=type,
+        date=snapshot_date,
     )
 
     return optionsChain
 
 
-def grab_optionprices(snapshot_date: str, optionsChain: pd.DataFrame):
-    allData = pd.DataFrame()
-    getMarketData = ivol.setMethod("/equities/eod/single-stock-option")
-
+def grab_optionprices(snapshot_date: str, optionsChain: pd.DataFrame, getMarketData: Any):
+    frames = []
     for optionSymbol in optionsChain["OptionSymbol"]:
         try:
             marketData = getMarketData(
                 symbol=optionSymbol,
-                from_=f"{snapshot_date}",
-                to=f"{snapshot_date}",
+                from_=snapshot_date,
+                to=snapshot_date,
                 delayBetweenRequests=1,
             )
-            allData = pd.concat([allData, marketData], axis=0)
+            frames.append(marketData)
         except requests.exceptions.RequestException as e:
             print(f"Error fetching data for {optionSymbol}: {e}")
-    return allData
+  
+    return pd.concat(frames, ignore_index=True)
 
 
-def collection(forward_dates: [], underlying: str):
-    # want to essentially loop over dates for a given underlying depending on a
-    # the specified forward date ie in the paper its 28 days or forward month
-    # this colleciton should just be a merge between calls and puts
-    data = pd.DataFrame()
-    for forward_date in forward_dates:
-        print(forward_date)
-        snapshot_date = forward_date - pd.Timedelta(days=7)
-        print(snapshot_date)
-        price = int(underlying_price(snapshot_date, underlying))
+def collection(dates: list[pd.Timestamp], underlying: str, getOptionsChain: Any, getMarketData: Any):
+    all_frames = []
+    start = time.time()
+    for snapshot_date in tqdm(dates):
+        forward_date = snapshot_date + pd.Timedelta(days=93)
+        expiry_start_date = snapshot_date + pd.Timedelta(days=3)
+        try:
+            price = int(underlying_price(snapshot_date, underlying))
+        except IndexError:
+            print(f"[WARN] No underlying price for {underlying} on {snapshot_date.date()}, skipping.")
+            continue     
+        except Exception as e:
+            print(f"[WARN] Error getting price for {underlying} on {snapshot_date.date()}: {e}")
+            continue
+
         symbols = grab_optionsymbol(
             snapshot_date=snapshot_date.strftime("%Y-%m-%d"),
+            expiry_start_date = expiry_start_date.strftime("%Y-%m-%d"),
             forward_date=forward_date.strftime("%Y-%m-%d"),
             option="SPX",
             type="C",
-            strikeFrom=price - 30,
-            strikeTo=price + 100,
-        )
-        option_NBBO = grab_optionprices(
-            snapshot_date=snapshot_date.strftime("%Y-%m-%d"), optionsChain=symbols
+            strikeFrom=price - 100,
+            strikeTo=price + 160,
+            getOptionsChain=getOptionsChain,
         )
 
-        data = pd.concat([data, option_NBBO], axis=0)
-    return data
+        option_NBBO = grab_optionprices(
+            snapshot_date=snapshot_date.strftime("%Y-%m-%d"),
+            optionsChain=symbols,
+            getMarketData=getMarketData,
+        )
+
+        all_frames.append(option_NBBO)
+    
+    end = time.time()
+    print("Data Collected in:", (end - start)/60, "minutes")
+
+    return pd.concat(all_frames, ignore_index=True) 
 
 
 def write(data: pd.DataFrame, year: str):
-    # Takes collection and writes into a csv to ensure if anything breaks along the lines
-    # we have the data and can continue as needed
-    if os.path.exists("data/raw_data_" + year):
-        return data.to_csv("data/raw_data_" + year, mode="a", header=False, index=False)
-    else:
-        return data.to_csv("data/raw_data_" + year, mode="w", header=False)
+    path = f"data/options_data_{year}.parquet"
+    data.to_parquet(path, index=False, compression="snappy")
+    print(f"File has been saved for {year}")
 
 
 def put_call_parity():
@@ -100,7 +106,7 @@ def put_call_parity():
 
 
 def underlying_price(date: str, underlying: str):
-    return yf.download(underlying, start=date, end=date + pd.Timedelta(days=1))[
+    return yf.download(underlying, start=date, end=date + pd.Timedelta(days=1),auto_adjust = True,progress=False)[
         "Close"
     ].values.flatten()[0]
 
@@ -109,24 +115,45 @@ def allfridays(year):
     return pd.date_range(start=str(year), end=str(year + 1), freq="W-FRI").tolist()
 
 
-# option_data = grab_optionsymbol(
-#     option="SPX",
-#     forward_date="2025-11-07",
-#     strikeFrom=6700,
-#     strikeTo=6750,
-#     type="C",
-#     snapshot_date="2025-10-31",
+
+# data_range = allfridays(2019)[:2]
+
+# retrieve = collection(
+#     forward_dates=data_range,
+#     underlying="^SPX",
 # )
 
-# test = grab_optionprices(snapshot_date="2025-10-31", optionsChain=option_data)
+# write(retrieve, "2019")
 
-# print(test)
-# print(print(test['date']),test['expiration'])
-data_range = allfridays(2019)
 
-retrieve = collection(
-    forward_dates=data_range,
-    underlying="^SPX",
-)
 
-write(retrieve, "2019")
+def main():
+    print(f"Loaded API_KEY (len={len(api_key) if api_key else 0})")
+
+    ivol.setLoginParams(username={user}, password={password})
+
+    ivol.setLoginParams(apiKey=api_key)
+    
+    getOptionsChain = ivol.setMethod(
+        "/equities/eod/option-series-on-date"
+    ) 
+
+    getMarketData = ivol.setMethod("/equities/eod/single-stock-option")
+
+    years = [2019,2020,2021,2022,2023,2024]
+
+    for year in years:
+        print("****Starting Api call****")
+        data_range = allfridays(year)
+
+        retrieve = collection(
+        dates=data_range,
+        underlying="^SPX",
+        getOptionsChain=getOptionsChain,
+        getMarketData=getMarketData
+    )
+        write(retrieve, str(year))
+
+
+if __name__ == "__main__":
+    main()
