65c076d4f3f50b67ae9468b1e4d7b314986ba760
MSchmidt99 <Maxfinnian@gmail.com>
2025-12-07 14:10:31 -0500

    hilbert cdf pricer

--- utils/risk_neutral_dist/GilPeleazEuropeanCDF.py
+++ utils/risk_neutral_dist/GilPeleazEuropeanCDF.py
@@ -2,6 +2,7 @@ import numpy as np
 from scipy.integrate import quad
 
 from fypy.pricing.fourier.GilPeleazEuropeanPricer import GilPeleazEuropeanPricer
+from fypy.pricing.fourier.HilbertEuropeanPricer import HilbertEuropeanPricer
 
 
 class GilPeleazEuropeanPricer_cdf(GilPeleazEuropeanPricer):
@@ -21,3 +22,22 @@ class GilPeleazEuropeanPricer_cdf(GilPeleazEuropeanPricer):
         int2 = 1 / 2 + 1 / np.pi * quad(integrand2, 1e-15, np.inf, limit=self._limit)[0]
 
         return 1 - int2
+
+
+class HilbertEuropeanPricer_cdf(HilbertEuropeanPricer):
+    def risk_neutral_cdf(self, T, K, is_call):
+        gridL = np.arange(-int(self._N / 2), 0)
+        gridR = -gridL[::-1]
+        H = (
+            np.sum(
+                self._g(self._h * gridL, T, K) * (np.cos(np.pi * gridL) - 1) / gridL
+                + self._g(self._h * gridR, T, K) * (np.cos(np.pi * gridR) - 1) / gridR
+            )
+            / np.pi
+        )
+        return -np.real(1j * H)
+
+    def _g(self, xi: np.ndarray, T: float, K: float):
+        return np.exp(-1j * xi * np.log(K / self._model.spot())) * (
+            self._model.chf(T, xi)
+        )
